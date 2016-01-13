fcaf7f29daf82ce880dbd00f409530c482c15e95
Dipak Suthar <dipaksuthar.gvp@gmail.com>
2016-01-13 13:28:53 +0530

    Add comment

--- download_image.py
+++ download_image.py
@@ -7,14 +7,14 @@ if len(sys.argv)>1: #Check for arguments for url list file
     for line in f:
         i = i + 1
         file_name = str(line).split('/')[-1]
-        download_dir = '/home/software/Desktop/'
+        download_dir = '/home/software/Desktop/'#Set default download path for local machine
         if os.path.exists(download_dir+str(file_name)) :
             f_name = file_name[:str(file_name).rfind('.')]
-            f_ext = file_name[str(file_name).rfind('.'):]
+            f_ext = file_name[str(file_name).rfind('.'):]#Added logic if both file name are same.
             file_name = f_name + '_'+str(i) + f_ext
         img = open(download_dir+str(file_name),'wb')
         try:
-            img.write(urllib.urlopen(line).read())
+            img.write(urllib.urlopen(line).read()) # Download image from url and write into local file.
         except:
             print "Url is not supported..."
         img.close()
